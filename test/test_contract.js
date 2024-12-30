4ab643f986e9f69645d7ea10a7df807ceb736a07
Carlos <cmunozg.dev@gmail.com>
2024-12-30 19:19:41 +0100

    added testing for addProposal()

--- test/test_contract.js
+++ test/test_contract.js
@@ -8,7 +8,7 @@
  * propuestas, votar y cerrar propuestas, para asegurarnos de que todo funciona como se espera.
  */
 
-const { Contract } = require("web3")
+const { Contract, InvalidPropertiesForTransactionTypeError } = require("web3")
 
 // Testeo del setup y deployment
 const VotingSystem = artifacts.require("VotingSystem")
@@ -22,6 +22,25 @@ contract("VotingSystem", (accounts => { // Accounts es un array de direcciones E
 
 	it("should set the correct owner of the VotingSystem instance", async() => {
 		const owner = await instance.owner(); // Llamamos al getter de owner 
-		assert.equal(owner, accounts[0], "Directions should match");
+		assert.equal(owner, accounts[0], "Account addresses should match");
+	})
+
+	it("should allow the owner to add a proposal, information should be correct", async() => {
+		//string memory propName, uint propDeadline
+		const deadlineString = "2025-12-12"
+		const deadlineObj = new Date(deadlineString)
+		const unixTime = Math.floor(deadlineObj.getTime() / 1000)
+		const proposalName = "Proposal 1"
+
+		await instance.addProposal(proposalName, unixTime)
+		const owner = await instance.owner();
+		const proposal = await instance.proposals(0);
+
+		assert.equal(proposalName, proposal.name, "Proposal names should match")
+		assert.equal(unixTime, proposal.deadline.toNumber(), "Proposal deadlines should match")
+		assert.equal(owner, proposal.proposedBy, "Account addresses should match")
+		assert.equal(0, proposal.votesInFavor.toNumber(), "Votes should be initialized to 0")
+		assert.equal(0, proposal.votesAgainst.toNumber(), "Votes should be initialized to 0")
+		assert.equal(true, proposal.isOpen, "Proposal should be initialized as open")
 	})
 }))
